94fe94904184be113d81c47a572cc56e85edfb1a
vprasad2280 <vprasad.2280@gmail.com>
2024-09-19 15:56:54 +0530

    This is first commit for CICD demo project

---
+++ src/test/java/TestCases/TestClass1.java
@@ -0,0 +1,46 @@
+package TestCases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import java.time.Duration;
+
+public class TestClass1 {
+    WebDriver driver;
+
+    @BeforeTest
+    public void LaunchApp(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-notifications");
+        driver = new ChromeDriver(options);
+        driver.get("https://testauto8-dev-ed.develop.my.salesforce.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+
+    @Test
+    public void LoginToApplication() throws InterruptedException {
+        driver.findElement(By.id("username")).sendKeys("testing@zensar.com.testauto");
+        driver.findElement(By.id("password")).sendKeys("Automation1"+ Keys.ENTER);
+        //driver.findElement(By.id("Login")).click();
+        Thread.sleep(3000);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+    @Test
+    public void VerifyPageTitle(){
+        String expectedTitle="Home | Salesforce";
+        String actualTitle=  driver.getTitle();
+        System.out.println("actualTitle = " + actualTitle);
+        Assert.assertEquals(actualTitle,expectedTitle);
+    }
+
+    @AfterTest
+    public void tearDown(){
+        driver.quit();
+    }
+}
